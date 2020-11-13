a32760ef506cb3fdb763e3a44bf87b01b0a75771
EQ <fleflefle1991@gmail.com>
2020-11-13 22:37:52 +0900

    Initial commit

---
+++ app/src/main/java/com/soldemom/parayo/intro/IntroActivity.kt
@@ -0,0 +1,10 @@
+package com.soldemom.parayo.intro
+
+import androidx.appcompat.app.AppCompatActivity
+
+class IntroActivity : AppCompatActivity() {
+
+
+
+
+}
---
+++ app/src/main/java/com/soldemom/parayo/intro/IntroActivityUI.kt
@@ -0,0 +1,11 @@
+package com.soldemom.parayo.intro
+
+
+//anko : 레이아웃을 xml 없이도 만들 수 있는 DSL임
+//class IntroActivityUI : AnkoComponent<IntroActivity> {
+//    override fun createView(ui: AnkoContext<IntroActivity>): View {
+//
+//
+//
+//    }
+//}
